d4c387d7c0ba8771dcbb00e14d726edcfae6e405
Kososhin <27874235+Kososhin@users.noreply.github.com>
2020-06-30 23:34:50 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router,
+  components:{App},
+  template: '<App />'
+})
