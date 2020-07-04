81122500ae7c00e064d6c2a63a0f1f85e3150108
Kososhin <27874235+Kososhin@users.noreply.github.com>
2020-07-05 05:17:13 +0800

    util.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store, //storeの定義
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
