78684bb36c64f6b4a8868b8e5eeaed0e81ae6c03
Kososhin <27874235+Kososhin@users.noreply.github.com>
2020-07-05 06:32:48 +0800

    storecheck

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
+Vue.config.devtools = true;
+
 import './bootstrap';
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+//ストアの読み込み
+import store from './store'
 
 new Vue({
   el: '#app',
